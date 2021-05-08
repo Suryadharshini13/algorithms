1cabcc3fd67b2a39419268043c07c130985d991e
surya-dharshini <suryadharshini13@gmail.com>
2021-05-08 15:23:10 +0530

    Reverse a String

--- addArray.java
+++ addArray.java
@@ -12,46 +12,46 @@ public class addArray {
             resultArr.add(sum%10);
         }
     }
-    public static void add(int[] arrA,int[] arrB,List<Integer> resultArr,int arrA_len,int arrB_len,int A_currPtr,int B_currPtr)
+    public static void add(int[] array_A,int[] array_B,List<Integer> resultArr,int array_A_len,int array_B_len,int A_currPtr,int B_currPtr)
     {
-        if(A_currPtr<arrA_len && B_currPtr< arrB_len)
+        if(A_currPtr<array_A_len && B_currPtr< array_B_len)
         {
-            int sum =arrA[A_currPtr]+arrB[B_currPtr];
+            int sum =array_A[A_currPtr]+array_B[B_currPtr];
             splitNum(resultArr,sum);
-            add(arrA,arrB,resultArr,arrA_len,arrB_len,A_currPtr+1,B_currPtr+1);
+            add(array_A,array_B,resultArr,array_A_len,array_B_len,A_currPtr+1,B_currPtr+1);
         }
-        else if(A_currPtr<arrA_len)
+        else if(A_currPtr<array_A_len)
         {
-            splitNum(resultArr,arrA[A_currPtr]);
-            add(arrA,arrB,resultArr,arrA_len,arrB_len,A_currPtr+1,B_currPtr+1);
+            splitNum(resultArr,array_A[A_currPtr]);
+            add(array_A,array_B,resultArr,array_A_len,array_B_len,A_currPtr+1,B_currPtr+1);
         }
-        else if(B_currPtr<arrB_len)
+        else if(B_currPtr<array_B_len)
         {
-            splitNum(resultArr,arrB[B_currPtr]);
-            add(arrA, arrB, resultArr, arrA_len, arrB_len, A_currPtr+1, B_currPtr+1);
+            splitNum(resultArr,array_B[B_currPtr]);
+            add(array_A, array_B, resultArr, array_A_len, array_B_len, A_currPtr+1, B_currPtr+1);
         }
         }
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
-        System.out.println("Enter the number of element in Array A");
-        int arrA_len = scan.nextInt();
-        System.out.println("Enter Array A elements");
-        int[] arrA = new int[arrA_len];
-        for(int i=0;i<arrA_len;i++)
+        System.out.println("Enter the number of element in array A");
+        int array_A_len = scan.nextInt();
+        System.out.println("Enter array A elements");
+        int[] array_A = new int[array_A_len];
+        for(int i=0;i<array_A_len;i++)
         {
-            arrA[i] = scan.nextInt();
+            array_A[i] = scan.nextInt();
         }
-        System.out.println("Enter the number of element in Array B");
-        int arrB_len = scan.nextInt();
-        System.out.println("Enter Array B elements");
-        int[] arrB = new int[arrB_len];
-        for(int i=0;i<arrB_len;i++)
+        System.out.println("Enter the number of element in array B");
+        int array_B_len = scan.nextInt();
+        System.out.println("Enter array B elements");
+        int[] array_B = new int[array_B_len];
+        for(int i=0;i<array_B_len;i++)
         {
-            arrB[i]=scan.nextInt();
+            array_B[i]=scan.nextInt();
         }
         List<Integer> resultArr = new ArrayList<>();
         int A_currPtr=0,B_currPtr=0;
-        add(arrA,arrB,resultArr,arrA_len,arrB_len,A_currPtr,B_currPtr);
+        add(array_A,array_B,resultArr,array_A_len,array_B_len,A_currPtr,B_currPtr);
         System.out.println(resultArr);
     }
 
