b4444e5a3be48806b8fb56845aa2d83fcfaddb75
Rubesh C V <rubesh.cv@gmail.com>
2021-05-10 20:41:08 +0530

    added the function

---
+++ StrStrFunction.java
@@ -0,0 +1,10 @@
+public class StrStrFunction {
+    public static int strstrFunction(String text1, String text2, int flag) {
+        if ()
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
