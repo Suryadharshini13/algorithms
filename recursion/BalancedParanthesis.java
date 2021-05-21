6e66f083ae60fd5f785be1a7f7d587625eeaf8eb
Rubesh C V <rubesh.cv@gmail.com>
2021-05-21 19:03:45 +0530

    BalancedParanthesis for k length

---
+++ recursion/BalancedParanthesis.java
@@ -0,0 +1,25 @@
+public class BalancedParanthesis {
+    // public static void balancedParanthesis(int n, int ob, int cb, String seq) {
+    //     if (n == 0) {
+    //         System.out.println(seq);
+    //         return;
+    //     }
+
+    //     if (ob > 0) {
+    //         balancedParanthesis(n-1, ob-1, cb, seq+"(");
+    //     }
+
+    //     if (cb > 0 && cb > ob) {
+    //         balancedParanthesis(n-1, ob, cb-1, seq+")");
+    //     }
+    // }
+
+    public static void balancedParanthesis(int n) {
+        
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        // balancedParanthesis(n, n/2, n/2, "");
+    }
+}
